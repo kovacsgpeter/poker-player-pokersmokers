9979aaa04548d7c2ac3bdff119654bc85d01c03e
Meszaros Gyorgy Attila <sirex1993@gmail.com>
2018-05-22 12:00:55 +0200

    check if have pair in community

--- player.py
+++ player.py
@@ -13,10 +13,13 @@ class Player:
 
         try:
             returnVal = 0
-            self.own_cards = self.get_own_cards(game_state)
+            own_cards = self.get_own_cards(game_state)
             isPair = Player.check_if_pair(self.own_cards)
             if isPair:
                 returnVal=200
+            if len(self.get_community_cards())>0:
+                if self.check_if_have_pair_incommunity(self.own_cards,self.get_community_cards()):
+                    returnVal=300
 
 
 
@@ -32,6 +35,20 @@ class Player:
             return True
         else:
             return False
+
+    def get_community_cards(game_state):
+        cards=[]
+        for card in game_state["community_cards"]:
+            cards.append(card)
+        return cards
+
+    def check_if_have_pair_incommunity(self,owncards, community_cards):
+        for card in owncards:
+            if card in community_cards:
+                return True
+            else:
+                continue
+        return False
 
 
 
