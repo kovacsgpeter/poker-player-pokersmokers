ae2ed4f8421478ee2b2b729405a40b853d62cc94
martonTorok <tormartonz@gmail.com>
2018-05-22 11:17:57 +0200

    +get_own_cards

--- player.py
+++ player.py
@@ -2,6 +2,15 @@ import json
 class Player:
     VERSION = "Default Python folding player"
 
+
+    def get_own_cards(self, game_state):
+        for player in game_state['players']:
+            try:
+                return player['hole_cards']
+            except KeyError:
+                pass
+
+
     def betRequest(self, game_state):
 
         if game_state['bet']>100:
