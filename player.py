2737f1fa1428eba765192d0c037039805644f85e
Peter <kovacs.g.peter@gmail.com>
2018-05-22 10:18:18 +0200

    pot-dependant bet

--- player.py
+++ player.py
@@ -1,9 +1,14 @@
-
+import json;
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        gamestat = json.load(game_state);
+        if gamestat['pot']>100:
+            return 0
+        else:
+            return 20
+
 
     def showdown(self, game_state):
         pass
